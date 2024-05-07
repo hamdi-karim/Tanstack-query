7e4392b4a5fc0ac673ec17e53632e0ed32401e2d
hamdi-karim <k.hamdi1995@gmail.com>
2024-05-07 12:45:25 +0100

    Show Loading & Error UI states in New events section

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -2,13 +2,14 @@ import { useMutation } from "@tanstack/react-query";
 import { Link, useNavigate } from "react-router-dom";
 
 import { createNewEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  const { mutate } = useMutation({
+  const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
   });
 
@@ -19,15 +20,27 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting...."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+
+      {isError && (
+        <ErrorBlock
+          title="Failed to create new event"
+          message={
+            error.info?.message || "Failed to create event. Please try again"
+          }
+        />
+      )}
     </Modal>
   );
 }
