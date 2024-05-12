1cd0ac19a0a65ccc856062ed35c7300760f8f20e
hamdi-karim <k.hamdi1995@gmail.com>
2024-05-12 12:09:25 +0100

    Fetching & Deleting an event data

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,51 +1,75 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link, Outlet, useParams } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import { fetchEvent } from "../../utils/http.js";
+import { deleteEvent, fetchEvent, queryClient } from "../../utils/http.js";
 import Header from "../Header.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["event "],
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({ id });
+  };
+
   let content;
 
-  if (isLoading) {
-    content = <LoadingIndicator />;
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching data for events</p>
+      </div>
+    );
   }
 
   if (isError) {
     content = (
       <ErrorBlock
-        title="ERROR"
-        message={error.info?.message || "Failed to load event data"}
+        title="Failed to Load events"
+        message={
+          error.info?.message || "Failed to load event data, please try again"
+        }
       />
     );
   }
 
   if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+
     content = (
       <article id="event-details">
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src={`http://localhost:3000/${data.image}`} alt="event image" />
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
           <div id="event-details-info">
             <div>
               <p id="event-details-location">{data.location}</p>
               <time dateTime={`${data.date}T${data.time}`}>
-                {data.date} {data.time}
+                {formattedDate} @ {data.time}
               </time>
             </div>
             <p id="event-details-description">{data.description}</p>
